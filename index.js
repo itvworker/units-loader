be4e6437145d241f89778517d122a5921b0753dc
谢克械 <ex_xiekx1@midea.com>
2019-10-09 18:03:38 +0800

    更新

--- index.js
+++ index.js
@@ -3,44 +3,56 @@ module.exports = function (source) {
     // const options = loaderUtils.getOptions(this)
     let opt = loaderUtils.getOptions(this)
 
-    let unit = 'rem';
-    let divisor = 37.5;
-    let raw = 'rpx';
-    let accuracy = 2;
+    // let unit = 'rem';
+    // let divisor = 37.5;
+    // let raw = 'rpx';
+    // let accuracy = 2;
 
 
-    if(opt && opt.unit){
-        unit = opt.unit;
+    let defualt_con = {
+        unit: 'rem',
+        divisor: 37.5,
+        raw: 'rpx',
+        accuracy: 2
     }
 
-    if(opt && opt.divisor){
-        divisor = opt.divisor;
+
+    if(opt.arr) {
+        let arr = opt.arr;
+        arr.forEach((item,index)=>{
+            let change = JSON.parse(JSON.stringify(defualt_con));
+            for(let i in item) {
+                change[i] = item[i]
+            }
+            let byte = 1;
+            for(let i =0, l = change.accuracy; i < l; i++) {
+                byte +='0';
+            }
+            byte = parseInt(byte);
+            let reg = new RegExp(`([0-9]+${change.raw})|([0-9]+\\.[0-9]*${change.raw})`,'g')
+            source = source.replace(reg, function(word){
+                return parseInt(parseFloat(word.slice(0,-change.raw.length))/change.divisor*byte)/byte+change.unit
+            })
+        })
+
+
+    }else{
+        let change = JSON.parse(JSON.stringify(defualt_con));
+        if(opt){
+            for(let i in opt) {
+                change[i] = opt[i]
+            }
+        }
+        let byte = 1;
+        for(let i =0, l = change.accuracy; i < l; i++) {
+            byte +='0';
+        }
+        byte = parseInt(byte);
+        let reg = new RegExp(`([0-9]+${change.raw})|([0-9]+\\.[0-9]*${change.raw})`,'g')
+        source = source.replace(reg, function(word){
+           return parseInt(parseFloat(word.slice(0,-change.raw.length))/change.divisor*byte)/byte+change.unit
+        })
     }
-
-    if(opt && opt.raw){
-        raw = opt.raw;
-    }
-
-    if(opt && opt.divisor){
-        divisor = opt.divisor;
-    }
-
-    if(opt && opt.accuracy){
-        accuracy = opt.accuracy;
-    }
-
-    let byte = 1;
-    for(let i =0, l = accuracy; i < l; i++) {
-        byte +='0';
-    }
-    byte = parseInt(byte);
-
-    let reg = new RegExp(`([0-9]+${raw})|([0-9]+.[0-9]?${raw})`,'g')
-    // let reg = /([0-9]+rpx)|([0-9]+.[0-9]?rpx)/g;
-
-    source = source.replace(reg, function(word){
-        return parseInt(parseFloat(word.slice(0,-3))/divisor*byte)/byte+unit
-    })
 
     return source
 }
